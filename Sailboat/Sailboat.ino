66c776c1ee1bc7ab603d83036bd93f4f38434a0c
leostti <titouan.leost@ensta-bretagne.org>
2024-06-17 13:29:44 +0100

    added while loops in setup to wait for a signal of the remote controller

--- Sailboat/Sailboat.ino
+++ Sailboat/Sailboat.ino
@@ -7,8 +7,8 @@
 #include <RCReceiver.h>
 
 Observer obs;
-Supervisor sp;
 Controller ctrl;
+Supervisor sp;
 Logger logger;
 RCReceiver rc;
 
@@ -21,17 +21,25 @@ void checkRC() {rc.checkRC();}
 
 void setup() {
     Serial.begin(9600);
-    obs.init();
-    rc.init();
-    sp.init(&obs, &ctrl, &rc);
-    ctrl.init();
-    logger.init(&obs);
 
-    pinMode(RUDDER_CH_PIN, INPUT);
-    pinMode(SAIL_CH_PIN, INPUT);
+    rc.init();
+    attachInterrupt(digitalPinToInterrupt(RUDDER_CH_PIN), checkRC, CHANGE);
+    delay(2000);
+    // This bloc of code is used to wait for the RC receiver to be ready.
+    // The first two while loops are passed when the RC Receiver is wired on the Arduino board.
+    // The last two while loops are passed when the remote controller is switched on then off.
+    // It allows to wired all the sensors on the board directly in the boat and then launch the program only when the user is ready.
+    while(!rc.isReceiving());
+    while(rc.isReceiving());
+    while(!rc.isReceiving());
+    while(rc.isReceiving());
+
+    obs.init();
+    ctrl.init(&obs);
+    sp.init(&obs, &ctrl, &rc);
+    logger.init(&obs, &ctrl, &rc);
 
     attachInterrupt(digitalPinToInterrupt(WIND_SPEED_PIN), anemometerRotation, FALLING);
-    attachInterrupt(digitalPinToInterrupt(RUDDER_CH_PIN), checkRC, CHANGE);
 
     last_time = millis();
 }
@@ -43,39 +51,43 @@ void loop() {
     ctrl.updateServos();
     logger.update();
 
-    // if (millis() - last_time > 200) {
-    //     Serial.print("Yaw (filtered): ");
-    //     Serial.print(obs.cmps()->getYaw());
-    //     Serial.print("     Yaw (raw): ");
-    //     Serial.print(obs.cmps()->getYawRaw());
-    //     Serial.print("     Pitch: ");
-    //     Serial.print(obs.cmps()->getPitch());
-    //     Serial.print("     Roll: ");
-    //     Serial.println(obs.cmps()->getRoll());
+    if (millis() - last_time > 200) {
+        Serial.print("Yaw (filtered): ");
+        Serial.print(obs.cmps()->getYaw());
+        Serial.print("     Yaw (raw): ");
+        Serial.print(obs.cmps()->getYawRaw());
+        Serial.print("     Pitch: ");
+        Serial.print(obs.cmps()->getPitch());
+        Serial.print("     Roll: ");
+        Serial.println(obs.cmps()->getRoll());
 
-    //     Serial.print("Satellites: ");
-    //     Serial.print(obs.gps()->getSatellites());
-    //     Serial.print("     Lat: ");
-    //     Serial.print(obs.gps()->getLat());
-    //     Serial.print("     Lon: ");
-    //     Serial.print(obs.gps()->getLon());
-    //     Serial.print("     Course: ");
-    //     Serial.print(obs.gps()->getCourse());
-    //     Serial.print("     Speed: ");
-    //     Serial.print(obs.gps()->getSpeed());
-    //     Serial.print("     Date: ");
-    //     Serial.print(obs.gps()->getDate());
-    //     Serial.print("     Time: ");
-    //     Serial.println(obs.gps()->getTime());
+        Serial.print("Satellites: ");
+        Serial.print(obs.gps()->getSatellites());
+        Serial.print("     Lat: ");
+        Serial.print(obs.gps()->getLat(), 6);
+        Serial.print("     Lon: ");
+        Serial.print(obs.gps()->getLon(), 6);
+        Serial.print("     Course: ");
+        Serial.print(obs.gps()->getCourse());
+        Serial.print("     Speed: ");
+        Serial.print(obs.gps()->getSpeed());
+        Serial.print("     Date: ");
+        Serial.print(obs.gps()->getDate());
+        Serial.print("     Time: ");
+        Serial.println(obs.gps()->getTime());
 
-    //     Serial.print("Wind speed: ");
-    //     Serial.print(obs.ws()->getWindSpeed());
-    //     Serial.print("     Wind direction: ");
-    //     Serial.println(obs.wd()->getWindDirection());
+        Serial.print("Wind speed: ");
+        Serial.print(obs.ws()->getWindSpeed());
+        Serial.print("     Wind direction (filtered): ");
+        Serial.print(obs.wd()->getWindDirection());
+        Serial.print("     Wind direction (raw): ");
+        Serial.print(obs.wd()->getRawWindDirection());
+        Serial.print("     True wind direction: ");
+        Serial.println(obs.getTrueWindAngle());
 
-    //     Serial.println("------------------------------- \n");
+        Serial.println("------------------------------- \n");
 
-    //     last_time = millis();
-    // }
+        last_time = millis();
+    }
 
 }
