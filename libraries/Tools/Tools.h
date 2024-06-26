f845988071595fb8409ea0380c05b56bc81db271
leostti <titouan.leost@ensta-bretagne.org>
2024-06-26 11:26:47 +0100

    added documentation to Tools.h

--- libraries/Tools/Tools.h
+++ libraries/Tools/Tools.h
@@ -6,10 +6,30 @@
 #include <Config.h>
 
 
+/***************************************************************
+ * Convert a latitude and a longitude to a XY coordinate.
+ * This function converts a latitude and a longitude to a XY 
+ * coordinate using the reference latitude and longitude.
+ * @param coordLatLon The latitude and longitude to convert
+ * @return The XY coordinate
+***************************************************************/
 CoordXY latLonToXY(CoordLatLon coordLatLon);
 
+/***************************************************************
+ * Sawtooth function.
+ * This function returns the sawtooth function of x.
+ * @param x The input value
+ * @return The sawtooth function of x
+***************************************************************/
 float sawtooth(float x);
 
+/***************************************************************
+ * Wrap a value between -180 and 180.
+ * Maps the real number line onto the interval [−180,180) 
+ * modulo 360.
+ * @param x The value to wrap
+ * @return The wrapped value
+***************************************************************/
 float wrap360(float x);
 
 #endif
