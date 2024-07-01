f86eae00f08915242691d24ea9ca474638bc8111
leostti <titouan.leost@ensta-bretagne.org>
2024-07-01 14:43:09 +0100

    improved plots + added video for the boat trajectory

--- tools/display_log.py
+++ tools/display_log.py
@@ -2,6 +2,7 @@ import os
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.patches as patches
+import matplotlib.animation as animation 
 
 
 def readLog(file):
@@ -77,6 +78,10 @@ def readLog(file):
 
     timestamp = [t - timestamp[0] for t in timestamp]
 
+    for i in range(int(nb_wp)):
+        wp_x, wp_y = latLonToXY(wp[i][0], wp[i][1], ref)
+        wp[i] = (wp_x, wp_y)
+
 
     ######################################
     ########### Save the data ############
@@ -105,6 +110,20 @@ def readLog(file):
     data.append(wp_radius)
 
     return data
+
+
+def latLonToXY(lat, lon, ref):
+    REF_LAT = ref[0]
+    REF_LON = ref[1]
+    EARTH_RADIUS = 6371000
+
+    if lat == 99999 or lon == 99999:
+        return 0, 0
+
+    x = EARTH_RADIUS * np.cos(lat*np.pi/180) * (lon - REF_LON) * np.pi/180
+    y = EARTH_RADIUS * (lat - REF_LAT) * np.pi/180
+
+    return x, y
 
 
 def plotYaw(data, date, time):
@@ -173,6 +192,20 @@ def plotXY(data, date, time):
     # Plot the remaining data (useful if status doesn't change)
     plt.plot(data[12][indice:], data[13][indice:], color=colors[val], ls=linestyles[val], label=labels[val] if label_added <= 1 else None)
 
+    # Waypoints display
+    wp_list = data[18]
+    nb_wp = int(data[19])
+    wp_radius = float(data[20])
+
+    for i in range(nb_wp):
+        plt.plot(wp_list[i][0], wp_list[i][1], 'yo')
+        circle = patches.Circle((wp_list[i][0], wp_list[i][1]), wp_radius, fill=False)
+        plt.gca().add_patch(circle)
+        if i > 0:
+            plt.plot([wp_list[i-1][0], wp_list[i][0]], [wp_list[i-1][1], wp_list[i][1]], 'g')
+        else:
+            plt.plot([data[12][0], wp_list[i][0]], [data[13][0], wp_list[i][1]], 'g')
+
     label_added = False
     for i, sat in enumerate(data[7][:]):
         if sat < 3:
@@ -181,15 +214,6 @@ def plotXY(data, date, time):
                 label_added = True
             else:
                 plt.plot(data[12][i], data[13][i], 'rx')
-
-    wp_list = data[18]
-    nb_wp = int(data[19])
-    wp_radius = float(data[20])
-    for i in range(nb_wp):
-        x, y = latLonToXY(wp_list[i][0], wp_list[i][1], data[17])
-        plt.plot(x, y, 'yo')
-        circle = patches.Circle((x, y), wp_radius, fill=False)
-        plt.gca().add_patch(circle)
 
     plt.legend()
 
@@ -237,7 +261,85 @@ def plotCmd(data, date, time):
     plt.savefig(f'logs/plots/{date}/{time}/cmd.png')
 
 
-def displayLog(file, date, time):
+def create_vid(data, date, time):
+    x = data[12]
+    y = data[13]
+    wp_list = data[18]
+    nb_wp = int(data[19])
+    wp_radius = float(data[20])
+    sat = data[7]
+    wd = data[5]
+    ws = data[6]
+    timestamp = data[0]
+
+    fig = plt.figure()
+    xmin = min(np.min(x), np.min([wp[0] for wp in wp_list]) - wp_radius) - 5
+    xmax = max(np.max(x), np.max([wp[0] for wp in wp_list]) + wp_radius) + 5
+    ymin = min(np.min(y), np.min([wp[1] for wp in wp_list]) - wp_radius) - 5
+    ymax = max(np.max(y), np.max([wp[1] for wp in wp_list]) + wp_radius) + 5
+    ax = plt.axes(xlim=(xmin, xmax), ylim=(ymin, ymax))
+    ax.set_xlabel('X (m)')
+    ax.set_ylabel('Y (m)')
+    ax.set_title('Sailboat Trajectory')
+
+    # Waypoints display
+    for i in range(nb_wp):
+        ax.plot(wp_list[i][0], wp_list[i][1], 'yo')
+        circle = patches.Circle((wp_list[i][0], wp_list[i][1]), wp_radius, fill=False)
+        ax.add_patch(circle)
+        if i > 0:
+            ax.plot([wp_list[i-1][0], wp_list[i][0]], [wp_list[i-1][1], wp_list[i][1]], 'g')
+        else:
+            ax.plot([x[0], wp_list[i][0]], [y[0], wp_list[i][1]], 'g')
+
+    # Add the north arrow
+    arrow_north = patches.FancyArrowPatch((0.93, 0.85), (0.93, 0.93), mutation_scale=20, color='black', arrowstyle='wedge', transform=ax.transAxes)
+    ax.add_patch(arrow_north)
+    ax.text(0.93, 0.95, 'North', transform=ax.transAxes, ha='center', va='center', fontsize=12, color='black')
+
+    # Initialize the animation
+    full_path, = ax.plot([], [], lw = 2, c='grey', alpha=0.5, label='Boat path')
+    path, = ax.plot([], [], lw = 2, c='r')
+    last_point, = ax.plot([], [], 'ko')
+    lost_sat, = ax.plot([], [], 'rx', label='Lost satellites')
+    wind_text = ax.text(0.03, 0.97, '', transform=ax.transAxes, ha='left', va='top', fontsize=8, c='k', bbox=dict(facecolor='red', alpha=0.5))
+
+    def init():
+        full_path.set_data([], [])
+        path.set_data([], [])
+        last_point.set_data([], [])
+        lost_sat.set_data([], [])
+        return full_path, path, last_point, lost_sat
+
+    xdata, ydata = [], []
+    xsat, ysat = [], []
+
+    def animate(i):
+        xdata.append(x[i])
+        ydata.append(y[i])
+        full_path.set_data(xdata, ydata)
+        path.set_data(xdata[-20:], ydata[-20:])
+        last_point.set_data(x[i], y[i])
+        if sat[i] < 3:
+            xsat.append(x[i])
+            ysat.append(y[i])
+            lost_sat.set_data(xsat, ysat)
+
+        wind_text.set_text(f'Wind direction: {wd[i]}°\nWind speed: {ws[i]:.2f} kph\nTime: {timestamp[i]:.1f}s')
+
+        return full_path, path, last_point, lost_sat
+    
+    ax.legend(loc='lower left')
+
+    # calling the animation function	 
+    anim = animation.FuncAnimation(fig, animate, init_func = init, 
+                                frames = len(x), interval = 20, blit = True) 
+
+    # saves the animation in our desktop 
+    anim.save(f'logs/plots/{date}/{time}/videoxy.mp4', writer = 'ffmpeg', fps = 60) 
+
+
+def displayLog(file, date, time, video=False):
     data = readLog(file)
 
     output_dir = f'logs/plots/{date}/{time}'
@@ -249,28 +351,24 @@ def displayLog(file, date, time):
     plotXY(data, date, time)
     plotCmd(data, date, time)
 
+    print(f'Log successfully saved in logs/plots/{date}/{time}/')
+
     plt.show()
 
-    print('Log displayed and saved successfully')
-
-
-def latLonToXY(lat, lon, ref):
-    REF_LAT = ref[0]
-    REF_LON = ref[1]
-    EARTH_RADIUS = 6371000
-
-    if lat == 99999 or lon == 99999:
-        return 0, 0
-
-    x = EARTH_RADIUS * np.cos(lat*np.pi/180) * (lon - REF_LON) * np.pi/180
-    y = EARTH_RADIUS * (lat - REF_LAT) * np.pi/180
-
-    return x, y
+    if video:
+        print('Creating video...')
+        create_vid(data, date, time)
+        print(f'Video created and saved successfully as logs/plots/{date}/{time}/videoxy.mp4')
 
 
 if __name__ == '__main__':
     date = input('Enter the date of the log in the format DDMMYY: ')
     time = input('Enter the time of the log in the format HHMMSS: ')
     file = open(f'logs/{date}/{time}.txt', 'r')
-    displayLog(file, date, time)
+    video = input('Do you want to create a video of the log? (y/n) ')
+    if video == 'y':
+        video = True
+    else:
+        video = False
+    displayLog(file, date, time, video)
     file.close()
